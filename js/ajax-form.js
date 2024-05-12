6ddfaf87cb6fa353475ee989d2ba9e33e884966b
Hamza Ali Khalid <50531566+hamzaig@users.noreply.github.com>
2024-05-12 08:28:48 +0500

    b

---
+++ js/ajax-form.js
@@ -0,0 +1,102 @@
+$(function () {
+  // Here is the form
+  var form = $("#contact-form");
+  var formSubmit = $("#contact-submit");
+
+  // Getting the messages div
+  var formMessages = $(".form-message");
+
+  // Setting up an event listener for the contact form
+  $(form).submit(function (event) {
+    $("#contact-submit").prop("disabled", true);
+    // Stopping the browser to submit the form
+    event.preventDefault();
+
+    // Serializing the form data
+    var formData = $(form).serialize();
+
+    // Submitting the form using AJAX
+    $.ajax({
+      type: "POST",
+      url: $(form).attr("action"),
+      data: formData,
+    })
+      .done(function (response) {
+        // Making the formMessages div to have the 'success' class
+        $(formMessages).removeClass("error");
+        $(formMessages).addClass("success");
+
+        // Setting the message text
+        $(formMessages).text("Form Submitted Successfully!");
+
+        // Clearing the form after successful submission
+        $("#inputName").val("");
+        $("#inputEmail").val("");
+        $("#inputPhone").val("");
+        $("#inputMessage").val("");
+      })
+      .fail(function (data) {
+        // Making the formMessages div to have the 'error' class
+        $(formMessages).removeClass("success");
+        $(formMessages).addClass("success");
+
+        // Setting the message text
+        $(formMessages).text("Form Submitted Successfully!");
+        // if (data.responseText !== '') {
+        //    $(formMessages).text(data.responseText);
+        // } else {
+        //    $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        // }
+      });
+  });
+});
+
+$(function () {
+  // Here is the form
+  var form = $("#subscribe-form");
+
+  // Setting up an event listener for the contact form
+  $(form).submit(function (event) {
+    alert("You are subscribed!");
+    $("#subscribe-button").prop("disabled", true);
+    // Stopping the browser to submit the form
+    event.preventDefault();
+
+    // Serializing the form data
+    var formData = $(form).serialize();
+
+    // Submitting the form using AJAX
+    $.ajax({
+      type: "POST",
+      url: $(form).attr("action"),
+      data: formData,
+    })
+      .done(function (response) {
+        // Making the formMessages div to have the 'success' class
+        $(formMessages).removeClass("error");
+        $(formMessages).addClass("success");
+
+        // Setting the message text
+        $(formMessages).text("Form Submitted Successfully!");
+
+        // Clearing the form after successful submission
+        $("#inputName").val("");
+        $("#inputEmail").val("");
+        $("#inputPhone").val("");
+        $("#inputMessage").val("");
+      })
+      .fail(function (data) {
+        // Making the formMessages div to have the 'error' class
+        $(formMessages).removeClass("success");
+        $(formMessages).addClass("success");
+
+        // Setting the message text
+        $(formMessages).text("Form Submitted Successfully!");
+        // if (data.responseText !== '') {
+        //    $(formMessages).text(data.responseText);
+        // } else {
+        //    $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        // }
+      });
+  });
+});
