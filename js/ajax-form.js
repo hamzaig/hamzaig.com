e32e34f7fd69edad163c3eafdcec442a81915631
Hamza Ali Khalid <hamzaig@yahoo.com>
2025-11-23 00:29:14 +0500

    x

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -29,6 +29,10 @@ $(function () {
       dataType: "json",
     })
       .done(function (response) {
+        // Clear any previous field errors
+        $(".field-error").remove();
+        $(".form-field-error").removeClass("form-field-error");
+        
         // Making the formMessages div to have the 'success' class
         $(formMessages).removeClass("error");
         $(formMessages).addClass("success");
@@ -52,25 +56,78 @@ $(function () {
         $("#inputConsent").prop("checked", false);
         $("#contact-submit").prop("disabled", false);
       })
-      .fail(function (data) {
+      .fail(function (xhr) {
         // Making the formMessages div to have the 'error' class
         $(formMessages).removeClass("success");
         $(formMessages).addClass("error");
+        
+        // Clear previous field errors
+        $(".field-error").remove();
+        $(".form-field-error").removeClass("form-field-error");
+        
         // Setting the message text
         try {
-          var errorResponse = typeof data.responseJSON !== 'undefined' ? data.responseJSON : 
-                             (typeof data.responseText !== 'undefined' && data.responseText !== '' ? 
-                              JSON.parse(data.responseText) : null);
-          if (errorResponse && errorResponse.error) {
-            $(formMessages).text(errorResponse.error);
-          } else if (errorResponse && errorResponse.message) {
-            $(formMessages).text(errorResponse.message);
+          var errorResponse = xhr.responseJSON || 
+                             (xhr.responseText ? JSON.parse(xhr.responseText) : null);
+          
+          if (errorResponse) {
+            // If there are specific field errors, display them
+            if (errorResponse.errors && Object.keys(errorResponse.errors).length > 0) {
+              var errorMessages = [];
+              
+              // Map field names to input IDs
+              var fieldMapping = {
+                'name': 'inputName',
+                'email': 'inputEmail',
+                'phone': 'inputPhone',
+                'company': 'inputCompany',
+                'serviceType': 'inputServiceType',
+                'budget': 'inputBudget',
+                'message': 'inputMessage',
+                'consent': 'inputConsent'
+              };
+              
+              // Display errors for each field
+              $.each(errorResponse.errors, function(field, message) {
+                var inputId = fieldMapping[field] || field;
+                var $field = $("#" + inputId);
+                
+                if ($field.length) {
+                  // Add error class to field
+                  $field.addClass("form-field-error");
+                  
+                  // Add error message below field
+                  var errorHtml = '<span class="field-error" style="color: red; font-size: 12px; display: block; margin-top: 5px;">' + message + '</span>';
+                  $field.after(errorHtml);
+                  
+                  errorMessages.push(message);
+                } else {
+                  errorMessages.push(message);
+                }
+              });
+              
+              // Show general error message with all errors
+              if (errorResponse.error) {
+                $(formMessages).html('<strong>Please fix the following errors:</strong><br>' + errorMessages.join('<br>'));
+              } else {
+                $(formMessages).html('<strong>Please fix the following errors:</strong><br>' + errorMessages.join('<br>'));
+              }
+            } else if (errorResponse.error) {
+              $(formMessages).text(errorResponse.error);
+            } else if (errorResponse.message) {
+              $(formMessages).text(errorResponse.message);
+            } else {
+              $(formMessages).text(
+                "Oops! An error occurred and your message could not be sent. Please try again."
+              );
+            }
           } else {
             $(formMessages).text(
               "Oops! An error occurred and your message could not be sent. Please try again."
             );
           }
         } catch (e) {
+          console.error('Error parsing response:', e);
           $(formMessages).text(
             "Oops! An error occurred and your message could not be sent. Please try again."
           );
